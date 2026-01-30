9fa8ac492f7cf5ec9926991b5fa11cf9e1b6bab7
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-01-30 10:43:46 +0100

    Add files via upload

---
+++ memory.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+	
+int main()
+{
+	srand(time(0));
+	int d;
+	
+	do
+	{
+		cout << "inserire la dimensione pari della matrice: ";
+		cin >> d;
+		if( d % 2 != 0 )
+			cout << "il numero inserito e' dispari\n";
+			
+	}while( d % 2 != 0 );
+	
+	int m[d][d];
+	
+	
+	
+	
+	
+	
+	//inserire dimensione se dispari si riinserisce
+	
+	//generare le coppie
+	
+	//utente inserisce le posizioni
+	
+	//numero di tentativi
+	
+	
+	
+    return 0;
+}	
