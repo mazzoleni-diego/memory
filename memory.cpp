c6800513c30efddac9e4690ea045aaafd1b8bc5c
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-01-30 11:13:21 +0100

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -3,37 +3,18 @@
 #include <cstdlib>
 
 using namespace std;
+
 	
 int main()
 {
 	srand(time(0));
-	int d;
 	
-	do
-	{
-		cout << "inserire la dimensione pari della matrice: ";
-		cin >> d;
-		if( d % 2 != 0 )
-			cout << "il numero inserito e' dispari\n";
-			
-	}while( d % 2 != 0 );
+	// creazion della matrice
+	const int d=rand()%12+4;
+	char m[d][d];
 	
-	int m[d][d];
-	
-	
-	
-	
-	
-	
-	//inserire dimensione se dispari si riinserisce
-	
-	//generare le coppie
-	
-	//utente inserisce le posizioni
-	
-	//numero di tentativi
 	
 	
 	
     return 0;
-}	
+}
